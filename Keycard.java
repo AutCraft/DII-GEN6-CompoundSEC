48896a46bbb5e46fbd62032a0b6a9afc005c1caf
AutCraft <aut.anud@gmail.com>
2025-01-30 12:32:48 +0700

    Update Keycard

---
+++ Keycard.java
@@ -0,0 +1,15 @@
+public class Keycard {
+    private String FirstName;
+    private String LastName;
+    private String Id;
+    private String Role;
+
+    public void add(String FirstName, String LastName, String Role){
+        switch (){
+
+        }
+        this.FirstName = FirstName;
+        this.LastName = LastName;
+        this.Role = Role;
+    }
+}
